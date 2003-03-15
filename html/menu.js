ab7afdf456ba44fb30fa8883e98da16e7433fa1d
jchoyt <jchoyt>
2003-03-15 04:48:11 +0000

    added AutoHost link

--- html/menu.js
+++ html/menu.js
@@ -1,5 +1,6 @@
 document.write('<table width=100%><tr><td align=center>' );
-document.write('<a href=ahc.html>Home</a> | ');
+document.write('<a href=index.html>Home</a> | ');
+document.write('<a href=http://library.southern.edu/stars/stars.htm>Stars! AutoHost</a> | ');.
 document.write('<a href=ahclient.jar>Download</a> | ');
 document.write('<a href=mailto:jchoyt@users.sourceforge.net>Source</a> | ');
 document.write('<a href=ahc_license.html>License</a> | ');
@@ -8,4 +9,4 @@ document.write('<a href=mailto:jchoyt@users.sourceforge.net>Contact me</a> | ');
 document.write('<a href="https://sourceforge.net/tracker/?atid=546423&group_id=76218&func=browse">Feature Request</a> | ');
 document.write('<a href="https://sourceforge.net/tracker/?atid=546420&group_id=76218&func=browse">Bug Reports</a>');
 document.write('</td></tr></table>');
-
+document.write('<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=76218&amp;type=3" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a>');
