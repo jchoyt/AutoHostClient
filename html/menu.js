11289c23119d9a19ac6e49cfa812a408ffd9e34a
jchoyt <jchoyt>
2003-03-19 04:56:54 +0000

    updated for Sourceforge site

--- html/menu.js
+++ html/menu.js
@@ -1,12 +1,10 @@
 document.write('<table width=100%><tr><td align=center>' );
 document.write('<a href=index.html>Home</a> | ');
-document.write('<a href=http://library.southern.edu/stars/stars.htm>Stars! AutoHost</a> | ');.
-document.write('<a href=ahclient.jar>Download</a> | ');
-document.write('<a href=mailto:jchoyt@users.sourceforge.net>Source</a> | ');
+document.write('<a href="http://sourceforge.net/projects/autohostclient">SourceForge site</a> | ');
 document.write('<a href=ahc_license.html>License</a> | ');
 document.write('<a href=faq.html>FAQ</a> | ');
 document.write('<a href=mailto:jchoyt@users.sourceforge.net>Contact me</a> | ');
 document.write('<a href="https://sourceforge.net/tracker/?atid=546423&group_id=76218&func=browse">Feature Request</a> | ');
 document.write('<a href="https://sourceforge.net/tracker/?atid=546420&group_id=76218&func=browse">Bug Reports</a>');
 document.write('</td></tr></table>');
-document.write('<a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=76218&amp;type=3" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a>');
+
