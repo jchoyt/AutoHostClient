44d30f7ed072dc270dba2ba6c399367651b972c5
jchoyt <jchoyt>
2004-02-16 16:34:18 +0000

    cleaned up source code header

--- src/stars/ahcgui/OptionPanelFactory.java
+++ src/stars/ahcgui/OptionPanelFactory.java
@@ -1,17 +1,23 @@
 /*
- *  This file is part of the AutoHost Client - [What it does in brief]
- *  Copyright (c) 2003 Jeffrey Hoyt
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or any later version.
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * :mode=java:tabSize=4:indentSize=4:noTabs=true:
+ * :folding=indent:collapseFolds=0:wrap=none:maxLineLen=0:
+ *
+ * $Source$
+ * Copyright (C) 2004 jchoyt
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package stars.ahcgui;
 import java.awt.*;
